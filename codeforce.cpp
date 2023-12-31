302a18a9d8a20554146b4be8ec6f0ea2900d07d8
simplord05 <149597765+simplord05@users.noreply.github.com>
2023-12-31 10:37:59 +0700

    Add files via upload

---
+++ codeforce.cpp
@@ -0,0 +1,61 @@
+// #define _CRT_SECURE_NO_WARNINGS
+// #include <bits/stdc++.h>
+// #pragma GCC optimize ("O3")
+// #pragma GCC target ("sse4")
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using ordered_set = tree<int, null_type, std::less<int>, rb_tree_tag, tree_order_statistics_node_update>;
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+#include <cstring>
+#include <bitset>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <fstream>
+#include <numeric>
+#include <iomanip>
+#include <vector>
+#include <sstream>
+#include <set>
+#include <map>   
+#include <iomanip>
+#include <climits>
+using namespace std;
+
+#define filein() freopen("test.inp", "r", stdin);
+#define fileout() freopen("test.out", "w", stdout);
+#define fast() ios_base::sync_with_stdio(NULL);cin.tie(NULL);
+#define ll long long
+#define MOD 1000000007
+#define N 1000000
+
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x << " = " << x << '\n';
+#else
+#define debug(x...)
+#endif
+
+#define vi vector<int>
+#define vl vector<ll>
+#define pii pair<int, int>
+#define pli pair<ll, int>
+#define mp make_pair
+#define pb push_back
+#define fi first
+#define se second
+
+void solve(){
+    
+}
+
+int main(){
+    fast(); cin.exceptions(cin.failbit);
+    if("test.inp"){filein(); fileout();}
+    int t; cin >> t;
+    while(t--)
+        solve(); 
+    return 0;
+}
