4274ea369ac438392609f86f1cd939bc5f33a6ad
Kuro-orzz <149597765+Kuro-orzz@users.noreply.github.com>
2024-09-08 17:03:48 +0700

    Create ver4.cpp

---
+++ ver4.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define ll long long
+#define MOD (ll)(1e9+7)
+#define all(x) (x).begin(),(x).end()
+
+#define vi vector<int>
+#define pii pair<int, int>
+#define fi first
+#define se second
+
+void solve(){
+    
+}
+
+int main(){
+    ios_base::sync_with_stdio(NULL);cin.tie(NULL);
+    // cin.exceptions(cin.failbit);
+    #ifndef ONLINE_JUDGE
+    freopen("test.inp", "r", stdin);
+    freopen("test.out", "w", stdout);
+    #endif
+    // int t; cin >> t;
+    // while(t--)
+        solve();
+    cerr << "\nTime run: " << 1000 * clock() / CLOCKS_PER_SEC << "ms" << '\n';
+    return 0;
+}
